39d33d68881e8c4a9ea6afd09d3f8a5dd93d552f
Allen V <allenbnvtra.11@gmail.com>
2023-06-13 02:15:09 +0800

    updated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <div className='h-20 flex items-center justify-center'>Prepared By: Christine Bare</div>
+        <div className='h-20 flex items-center justify-center'>Prepared By: Christine Bare!</div>
     </div>
   );
 }
